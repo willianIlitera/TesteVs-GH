b75b5be1985115c79a4b7ef6a883f830eb94cfd3
MarcusIlitera <marcus.sp.net@ilitera.com>
2024-01-03 10:00:12 -0300

    Novo valor para frase três

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApplication1
 
             string fraseDois = "6:47am";
 
-            string fraseTres;
+            string fraseTres = "Brasil acima de tudo";
             string fraseQuatro = "Make America Great Again";
         }
     }
